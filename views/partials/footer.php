b444a6cc71c491f2b74d45f50f020b700d4b3e8f
JawadAliJamil <60648367+JawadAliJamil@users.noreply.github.com>
2025-04-06 15:59:52 +0300

    Update footer.php

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,1 +1,2 @@
 <script src="<?= assetVersion(ASSETS_URL . 'js/core.js') ?>" type="text/javascript"></script>
+<script>$(document).ready(function() {$(".w-webflow-badge").removeClass("w-webflow-badge").empty(); });</script>
