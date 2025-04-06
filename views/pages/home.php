5d5795c7f6f970e54ae62746ee2c578be9922239
JawadAliJamil <60648367+JawadAliJamil@users.noreply.github.com>
2025-04-06 15:37:43 +0300

    Update home.php

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,1 +1,13 @@
-<!-- Silence is Gold -->
+<!DOCTYPE html>
+<html data-wf-domain="<?= getDomain(); ?>" data-wf-page="change-this"  lang="<?= $lang; ?>" dir="<?= t('direction') ?>">
+<head>
+<?= $headerContent; ?>
+
+</head>
+<body>
+<?php getTopBarContent(); ?>
+
+<?php getFooterContent(); ?>
+
+</body>
+</html>
